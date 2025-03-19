9f2964d2684231f7d22f5daa294ef76e184eb3a6
Hasan Goli <mrhasangoli@gmail.com>
2025-03-19 22:47:47 +0330

    Header done

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,63 @@
+import { Button } from "@/components/ui/button";
+import {
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { SearchDialog } from "./search-dialog";
+import { ThemeToggle } from "./theme-toggle";
+
+const menuItems = [
+	{ title: "خانه", url: "/" },
+	{ title: "مقالات", url: "/blogs" },
+	{ title: "نویسندگان", url: "/authors" },
+	{ title: "تماس با ما", url: "/contact" },
+];
+
 export const Header = () => {
-	return <header className="container">Header</header>;
+	return (
+		<header className="container py-4">
+			<div className="grid grid-cols-3 items-center">
+				<div className="text-start">
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button variant="ghost">
+								<Menu />
+							</Button>
+						</SheetTrigger>
+						<SheetContent className="pt-8">
+							<SheetHeader>
+								<SheetTitle className="text-center">LOGO</SheetTitle>
+							</SheetHeader>
+							<nav className="px-4">
+								<ul className="flex flex-col gap-2">
+									{menuItems.map(item => (
+										<li
+											key={item.url}
+											className="p-2 border-border border-[1px] rounded-md text-sm">
+											<Link href={item.url}>{item.title}</Link>
+										</li>
+									))}
+									<li className="mt-4">
+										<Button className="w-full" asChild>
+											<Link href="/">ورود / عضویت</Link>
+										</Button>
+									</li>
+								</ul>
+							</nav>
+						</SheetContent>
+					</Sheet>
+				</div>
+				<h2 className="text-center">LOGO</h2>
+				<div className="flex items-center justify-end">
+					<SearchDialog />
+					<ThemeToggle />
+				</div>
+			</div>
+		</header>
+	);
 };
---
+++ src/components/layout/search-dialog.tsx
@@ -0,0 +1,40 @@
+import { Search } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+
+export const SearchDialog = () => {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button variant="ghost">
+					<Search />
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-md">
+				<DialogHeader>
+					<DialogTitle className="text-center">جستجو</DialogTitle>
+					<DialogDescription className="text-center">
+						عبارت مدنظر خود را وارد کنید
+					</DialogDescription>
+				</DialogHeader>
+				<form className="flex items-center space-x-2">
+					<div className="grid flex-1 gap-2">
+						<Input placeholder="جست و جو ..." />
+					</div>
+					<Button type="submit" size="sm">
+						<Search />
+					</Button>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+};
