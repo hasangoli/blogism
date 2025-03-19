db8dead784f3a80cced586c3491167f80eced59d
Hasan Goli <mrhasangoli@gmail.com>
2025-03-19 17:34:23 +0330

    Footer updated

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,5 @@
 import { Separator } from "@/components/ui/separator";
+import { Github, Instagram, Linkedin, Youtube } from "lucide-react";
 import Link from "next/link";
 import { Fragment } from "react";
 
@@ -7,10 +8,10 @@ const data = {
 	about:
 		"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطر آنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد،",
 	categories: [
-		{ name: "دسته بندی 1", slug: "test1" },
-		{ name: "دسته بندی 2", slug: "test2" },
-		{ name: "دسته بندی 3", slug: "test3" },
-		{ name: "دسته بندی 4", slug: "test4" },
+		{ name: "تکنولوژی", slug: "test1" },
+		{ name: "خدمات", slug: "test2" },
+		{ name: "علمی", slug: "test3" },
+		{ name: "صنعت", slug: "test4" },
 	],
 	links: [
 		{ title: "خانه", url: "/" },
@@ -26,6 +27,25 @@ const data = {
 	],
 };
 
+const socialIcon = (key: string) => {
+	switch (key) {
+		case "instagram":
+			return <Instagram />;
+
+		case "youtube":
+			return <Youtube />;
+
+		case "linkedin":
+			return <Linkedin />;
+
+		case "github":
+			return <Github />;
+
+		default:
+			break;
+	}
+};
+
 export const Footer = () => {
 	return (
 		<footer className="container">
@@ -38,11 +58,11 @@ export const Footer = () => {
 					<h4 className="inline-block pb-2 text-lg border-b-[1px] border-border text-bold mb-4">
 						دسته بندی ها
 					</h4>
-					<ul className="flex flex-wrap h-6 gap-x-3">
+					<ul className="flex flex-wrap h-6 gap-3">
 						{data.categories.map((category, index) => (
 							<Fragment key={index}>
 								{index ? <Separator orientation="vertical" /> : null}
-								<li>
+								<li className="border-[1px] border-border rounded-full text-xs py-1 px-2">
 									<Link href={category.slug}>{category.name}</Link>
 								</li>
 							</Fragment>
@@ -68,7 +88,7 @@ export const Footer = () => {
 					<ul className="flex flex-wrap gap-x-3">
 						{data.socials.map(social => (
 							<li key={social.url}>
-								<Link href={social.url}>{social.icon}</Link>
+								<Link href={social.url}>{socialIcon(social.icon)}</Link>
 							</li>
 						))}
 					</ul>
