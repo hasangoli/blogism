863065aa8d028115d50a26defd30879f15d340a6
Hasan Goli <mrhasangoli@gmail.com>
2025-03-23 20:12:21 +0330

    Author list done

--- src/components/authors/author-item.tsx
+++ src/components/authors/author-item.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent } from "../ui/card";
 
 export const AuthorItem = () => {
-  return (
-    <div>AuthorItem</div>
-  )
-}
+	return (
+		<Card>
+			<CardContent>
+				<Link
+					href="/authors/1"
+					className="flex flex-col gap-4 items-center justify-center">
+					<div className="relative aspect-square w-[100px]">
+						<Image
+							src="/images/test.jpg"
+							alt="author"
+							fill
+							className="rounded-full border-2 border-border"
+						/>
+					</div>
+					<p className="text-lg font-bold">حسن گلی</p>
+					<p className="text-sm text-center">25 مقاله</p>
+				</Link>
+			</CardContent>
+		</Card>
+	);
+};
