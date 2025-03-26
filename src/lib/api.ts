0aa6973c4c6599ac13fb8ff4c762a2537566785d
Hasan Goli <mrhasangoli@gmail.com>
2025-03-26 14:26:40 +0330

    Some changes applied

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,8 @@
-"use server";
-
-import { prisma } from "./prisma";
+// "use server";
 
 export const fetchSettings = async () => {
-	const settings = await prisma.settings.findFirst();
-	if (!settings) throw new Error("Failed to fetch settings");
-	return settings;
+	// const settings = await prisma.settings.findFirst();
+	console.log("api called");
+	// if (!settings) throw new Error("Failed to fetch settings");
+	// return settings;
 };
